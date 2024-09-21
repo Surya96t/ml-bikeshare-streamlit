42c7c805ca6da4d71831684062f3d1376f08da70
Surya96t <surya96t@gmail.com>
2024-09-20 20:46:46 -0400

    created config parameters

---
+++ bikeshare/configs/config.py
@@ -0,0 +1,47 @@
+CFGLog = {
+    "data": {
+        "path": "./data/SeoulBikeData.csv",
+        "X": [
+            'date', 'rented_bike_count',
+            'hour', 'temp', 'humidity',
+            'wind_speed', 'visibility', 
+            'dew_point_temp', 'solar_rad',
+            'rainfall', 'snowfall',
+            'seasons', 'holiday',
+            'functioning_day'
+        ],
+        "y": "rented_bike_count",
+        "test_size": 0.2,
+        "random_state": 42
+    },
+    "decision_tree": {
+        "crierion": "squared_error",
+        "max_depth": 20,
+        "max_features": None,
+        "max_leaf_nodes": None,
+        "min_samples_leaf": 2,
+        "min_samples_split": 10,
+        "splitter": "best"
+    },
+    "random_forest": {
+        "n_estimators": 300,
+        "max_depth": 30,
+        "max_features": "sqrt",
+        "min_samples_leaf": 1,
+        "min_samples_split": 2
+    },
+    "gradient_boosting": {
+        "n_estimators": 300,
+        "max_depth": None,
+        "max_features": "auto",
+        "min_samples_leaf": 1,
+        "min_samples_split": 2
+    },
+    "output": {
+        "output_path": "./artifacts/",
+        "dt_model": "decision_tree/dt_model.pkl",
+        "rf_model": "random_forest/rf_model.pkl",
+        "xgb_model": "xg_boost/xgb_model.pkl",
+    }
+}
+
